c678f4a1a181383e64b0cc21ccdcd5e63d240c69
maolizi <lizhen.mao@mijahealth.com>
2023-03-22 17:18:52 +0800

    feat: ✨修改测试

--- packages/button/src/demo/index.tsx
+++ packages/button/src/demo/index.tsx
@@ -2,7 +2,7 @@ import Button from '@pro-demo/button';
 import React from 'react';
 
 const Demo = () => {
-  return <Button name="button"></Button>;
+  return <Button name="button">123</Button>;
 };
 
 export default Demo;
